7e6d74137dda3fad58e9d9919e270d6995846ace
Vetal-Koz <kozvitalik567@gmail.com>
2023-09-21 21:22:51 +0300

    Did all work to 9 lesson

---
+++ src/main/java/com/rungroop/web/service/impl/ClubServiceImpl.java
@@ -0,0 +1,66 @@
+package com.rungroop.web.service.impl;
+
+import com.rungroop.web.dto.ClubDto;
+import com.rungroop.web.models.Club;
+import com.rungroop.web.repository.ClubRepository;
+import com.rungroop.web.service.ClubService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class ClubServiceImpl implements ClubService {
+    private ClubRepository clubRepository;
+
+    @Autowired
+    public ClubServiceImpl(ClubRepository clubRepository){
+        this.clubRepository = clubRepository;
+    }
+    @Override
+    public List<ClubDto> findAllClubs() {
+        List<Club> clubs = clubRepository.findAll();
+        return clubs.stream().map((club) -> mapToClubDto(club)).collect(Collectors.toList());
+    }
+
+    @Override
+    public Club saveClub(Club club) {
+        return clubRepository.save(club);
+    }
+
+    @Override
+    public ClubDto findClubById(Long clubId) {
+        Club club = clubRepository.findById(clubId).get();
+        return mapToClubDto(club);
+    }
+
+    @Override
+    public void updateClub(ClubDto clubDto) {
+        Club club = mapToClub(clubDto);
+        clubRepository.save(club);
+    }
+
+    private Club mapToClub(ClubDto club){
+        Club clubDto = Club.builder()
+                .id(club.getId())
+                .title(club.getTitle())
+                .photoUrl(club.getPhotoUrl())
+                .createOn(club.getCreateOn())
+                .updatedOn(club.getUpdatedOn())
+                .build();
+        return clubDto;
+    }
+
+    private ClubDto mapToClubDto(Club club){
+        ClubDto clubDto = ClubDto.builder()
+                .id(club.getId())
+                .title(club.getTitle())
+                .photoUrl(club.getPhotoUrl())
+                .content(club.getContent())
+                .createOn(club.getCreateOn())
+                .updatedOn(club.getUpdatedOn())
+                .build();
+        return clubDto;
+    }
+}
